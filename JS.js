02bae9cf64d0a516b98555a6c5accf3a42f45a0f
AlikeIFMA <christyannalike@acad.ifma.edu.br>
2024-07-16 16:18:45 -0300

    alteração 1

---
+++ JS.js
@@ -0,0 +1,9 @@
+function atualizarmenu(){
+    if (document.getElementsById('menuresponsivo').checked == true) {
+        (document.getElementsById('menu').style.borderBottomRightRadius = '0');
+        (document.getElementsById('menu').style.borderBottomLeftRadius = '0');
+
+    }else{
+        (document.getElementsById('menu').style.borderRadius = '3px');
+    }
+}
