5f054d7d771ebc433584ff91606a5695df35491f
AlikeIFMA <christyannalike@acad.ifma.edu.br>
2024-07-23 11:56:36 -0300

    alterações 1.1.3

--- JS.js
+++ JS.js
@@ -2,6 +2,8 @@ function atualizarmenu(){
     if (document.getElementsById('menuresponsivo').checked == true) {
         (document.getElementsById('menu').style.borderBottomRightRadius = '0');
         (document.getElementsById('menu').style.borderBottomLeftRadius = '0');
+        (document.getElementsById('texto').style.borderBottomRightRadius = '0');
+        (document.getElementsById('texto').style.borderBottomLeftRadius = '0');
 
     }else{
         (document.getElementsById('menu').style.borderRadius = '3px');
